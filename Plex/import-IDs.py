148c0f0afe670192539fa35a7175fc1b132b6f8c
Chaz Larson <chaz@chazlarson.com>
2023-01-23 00:01:23 -0600

    progress bar, cleanup

--- Plex/import-IDs.py
+++ Plex/import-IDs.py
@@ -1,33 +1,10 @@
+import ast
 import logging
-import os
-import re
-import platform
 from pathlib import Path
-import sys
-from pathvalidate import ValidationError, validate_filename
-
-import time
-
-from alive_progress import alive_bar
-from dotenv import load_dotenv
-import plexapi
-from plexapi.exceptions import Unauthorized
-from plexapi.server import PlexServer
-from plexapi.utils import download
-from helpers import booler, get_ids, validate_filename, get_plex
-
-import json
-import piexif
-import piexif.helper
-
-import filetype
-
-import requests
-import time
-from multiprocessing import cpu_count
-from multiprocessing.pool import ThreadPool
 
 import sqlalchemy as db
+from alive_progress import alive_bar
+from dotenv import load_dotenv
 from sqlalchemy.dialects.sqlite import insert
 
 CHANGE_FILE_NAME = "changes.txt"
@@ -159,57 +136,6 @@ logging.basicConfig(
 
 logging.info("Starting import-IDs.py")
 
-def get_IDs(type, item):
-    imdbid = None
-    tmid = None
-    tvid = None
-    raw_guid = item.guid
-    bits = raw_guid.split('/')
-    # plex://movie/5d776b59ad5437001f79c6f8
-    # local://3961921
-    if bits[0] == 'plex:':
-        try:
-            guid = bits[3]
-        
-            if guid not in COMPLETE_ARRAY:
-                try:
-                    if item.type != 'collection':
-                        logging.info("Getting IDs")
-                        imdbid, tmid, tvid = get_ids(item.guids, TMDB_KEY)
-                        complete = imdbid is not None and tmid is not None and tvid is not None 
-                        payload = {
-                            'guid': guid,
-                            'imdb': imdbid,
-                            'tmdb': tmid,
-                            'tvdb': tvid,
-                            'title': item.title,
-                            'year': item.year,
-                            'type': type,
-                            'complete': complete
-                        }
-
-                        diffs = get_diffs(payload)
-                            
-                        if diffs['new'] or diffs['updated']:
-                            # record change
-                            action = 'new' if diffs['new'] else 'updated'
-
-                            with open(change_file, "a", encoding="utf-8") as cf:
-                                cf.write(f"{guid} - {item.title} - {action} - {diffs['changes']} {os.linesep}")
-
-                            insert_record(payload)
-                except Exception as ex:
-                    print(f"{item.ratingKey}- {item.title} - Exception: {ex}")  
-                    logging.info(f"EXCEPTION: {item.ratingKey}- {item.title} - Exception: {ex}")
-            else:
-                logging.info(f"{guid} already complete")
-        except Exception as ex:
-            logging.info(f"No guid: {bits}")
-        
-def bar_and_log(the_bar, msg):
-    logging.info(msg)
-    the_bar.text = msg
-
 change_records = None
 
 COMPLETE_ARRAY = []
@@ -221,33 +147,32 @@ for thing in completed_things:
 
 with open(change_file, "r", encoding="utf-8") as cf:
     data = cf.read()
-    change_records = data.split("\n")
+    items = data.split("\n")
 
-import re
-import ast
+item_total = len(items)
 
-def get_type(type):
-    if type == 'movie':
-        return plexapi.video.Movie
-    if type == 'show':
-        return plexapi.video.Show
+with alive_bar(item_total, dual_line=True, title="Import changes") as bar:
+    for item in items:
 
-for thing in change_records:
-    parts = thing.strip().split(' - ')
-    # print(parts)
-    # ['63cc57ebfb50b5c850ea5c7b', 'Jai Kali Kalkattawali', 'new', "{'imdb': None, 'tmdb': '1076528', 'year': 2023}"]
-    if len(parts) == 2:
-        action = parts[0]
-        values = ast.literal_eval(parts[1])
-        payload = {}
+        parts = item.strip().split(' - ')
 
-        for key in values.keys():
-            payload[key] = values[key]
-                    
-        is_complete = payload['imdb'] is not None and payload['tmdb'] is not None and payload['tvdb'] is not None and payload['year'] is not None
-        
-        payload['complete'] = is_complete
-        
-        print(payload)
+        if len(parts) == 2:
+            action = parts[0]
+            values = ast.literal_eval(parts[1])
+            logging.info("================================")
+            logging.info(f"Importing {action} {values['guid']}")
+            bar.text = f"Importing {action} {values['guid']}"
+            payload = {}
 
-        insert_record(payload)
+            for key in values.keys():
+                payload[key] = values[key]
+                        
+            is_complete = payload['imdb'] is not None and payload['tmdb'] is not None and payload['tvdb'] is not None and payload['year'] is not None
+            
+            payload['complete'] = is_complete
+            
+            logging.info(f"{payload}")
+
+            insert_record(payload)
+
+        bar()
