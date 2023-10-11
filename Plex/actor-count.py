6f15a75611bc9e00824d356951034664f06b5698
Chaz Larson <chaz@chazlarson.com>
2023-10-11 17:34:46 -0500

    more logging

--- Plex/actor-count.py
+++ Plex/actor-count.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from pathlib import Path
 import platform
 import time
+from timeit import default_timer as timer
 from alive_progress import alive_bar
 from plexapi.server import PlexServer
 import os
@@ -12,12 +13,19 @@ import textwrap
 from tmdbapis import TMDbAPIs
 
 from helpers import booler, get_size, get_all_from_library, get_ids, get_letter_dir, get_plex, redact, validate_filename, load_and_upgrade_env
+from logs import setup_logger, plogger, blogger, logger
 
 # DONE 0.1.0: refactoring, added version
+
+start = timer()
 
 SCRIPT_NAME = Path(__file__).stem
 
 VERSION = "0.1.0"
+
+ACTIVITY_LOG = f"{SCRIPT_NAME}.log"
+
+setup_logger('activity_log', ACTIVITY_LOG)
 
 env_file_path = Path(".env")
 
@@ -29,27 +37,30 @@ IS_WINDOWS = platform.system() == "Windows"
 # convert to string
 RUNTIME_STR = now.strftime("%Y-%m-%d %H:%M:%S")
 
-print(f"Starting {SCRIPT_NAME} {VERSION} at {RUNTIME_STR}", 'info', 'a')
+plogger(f"Starting {SCRIPT_NAME} {VERSION} at {RUNTIME_STR}", 'info', 'a')
 
 if load_and_upgrade_env(env_file_path) < 0:
     exit()
 
 LIBRARY_NAME = os.getenv("LIBRARY_NAME")
 LIBRARY_NAMES = os.getenv("LIBRARY_NAMES")
+
+if LIBRARY_NAMES:
+    LIB_ARRAY = [s.strip() for s in LIBRARY_NAMES.split(",")]
+else:
+    LIB_ARRAY = [LIBRARY_NAME]
+
 TMDB_KEY = os.getenv("TMDB_KEY")
 TVDB_KEY = os.getenv("TVDB_KEY")
 CAST_DEPTH = int(os.getenv("CAST_DEPTH"))
 TOP_COUNT = int(os.getenv("TOP_COUNT"))
 ACTORS_ONLY = booler(os.getenv("ACTORS_ONLY"))
+TRACK_GENDER = booler(os.getenv("TRACK_GENDER"))
+
 DELAY = int(os.getenv("DELAY"))
 
 if not DELAY:
     DELAY = 0
-
-if LIBRARY_NAMES:
-    lib_array = LIBRARY_NAMES.split(",")
-else:
-    lib_array = [LIBRARY_NAME]
 
 tmdb = TMDbAPIs(TMDB_KEY, language="en")
 
@@ -71,7 +82,20 @@ def getTID(theList):
 
 plex = get_plex()
 
-for lib in lib_array:
+ALL_LIBS = plex.library.sections()
+ALL_LIB_NAMES = []
+
+plogger(f"{len(ALL_LIBS)} libraries found:", 'info', 'a')
+for lib in ALL_LIBS:
+    ALL_LIB_NAMES.append(f"{lib.title.strip()}")
+
+if LIBRARY_NAMES == 'ALL_LIBRARIES':
+    LIB_ARRAY = []
+    for lib in ALL_LIBS:
+        LIB_ARRAY.append(lib.title.strip())
+
+
+for lib in LIB_ARRAY:
     print(f"getting items from [{lib}]...")
     the_lib = plex.library.section(lib)
     items = get_all_from_library(plex, the_lib)
@@ -84,7 +108,8 @@ for lib in lib_array:
     skip_count = 0
     highwater_cast = 0
     total_cast = 0
-    with alive_bar(item_total, dual_line=True, title='Collection list - Plex') as bar:
+    average_cast = 0
+    with alive_bar(item_total, dual_line=True, title=f"Actor Count: {lib}") as bar:
         for item in items:
             tmpDict = {}
             tmdb_id, tvdb_id = getTID(item.guids)
@@ -98,18 +123,26 @@ for lib in lib_array:
                 count = 0
                 cast_size = len(cast)
                 if cast_size < 2:
-                    print(f"{item.title}: {cast_size}")
+                    print(f"small cast - {item.title}: {cast_size}")
                 casts[f"{cast_size}"] += 1
                 total_cast += cast_size
+                average_cast = round(total_cast / item_count)
                 if cast_size > highwater_cast:
                     highwater_cast = cast_size
-                    print(f"New cast size high water mark: {highwater_cast}")
+                    print(f"New cast size high water mark - {item.title}: {highwater_cast}")
 
+                bar.text(f"Processing {CAST_DEPTH if CAST_DEPTH < cast_size else cast_size} of {cast_size} from {item.title} - average cast size {average_cast}")
                 for actor in cast:
+                    # actor points to person
+                    gender = None
+                    if TRACK_GENDER:
+                        person = tmdb.person(actor.person_id)
+                        gender = person.gender
+
                     if count < CAST_DEPTH:
                         count = count + 1
                         cast_count += 1
-                        the_key = f"{actor.name} - {actor.person_id}"
+                        the_key = f"{actor.name} - {actor.person_id} - {gender}"
                         count_them = False
                         if ACTORS_ONLY:
                             if actor.known_for_department == "Acting":
@@ -124,7 +157,7 @@ for lib in lib_array:
                             actors[the_key] += 1
                             credit_count += 1
             except Exception as ex:
-                progress(item_count, item_total, "EX: " + item.title)
+                print(f"{item_count}, {item_total}, EX: {item.title}")
 
             bar()
 
@@ -133,7 +166,9 @@ for lib in lib_array:
 
     print("\r\r")
 
-    print(f"Looked at {cast_count} credits from the top {CAST_DEPTH} from each {the_lib.TYPE}")
+    end = timer()
+    elapsed = end - start
+    print(f"Looked at {cast_count} credits from the top {CAST_DEPTH} from each {the_lib.TYPE} in {elapsed} seconds.")
     print(f"Unique people: {len(actors)}")
     print(f"Unique cast counts: {len(casts)}")
     print(f"Longest cast list: {highwater_cast}")
